d72ba37b3184fb7bc97c17e2294784dc02b23938
antoniHerbert <adasilvafrancelino@gmail.com>
2025-01-31 11:06:14 -0300

    etc

--- src/components/SegmentsSelection.tsx
+++ src/components/SegmentsSelection.tsx
@@ -112,7 +112,162 @@ const Segmentos = forwardRef((props, ref) => {
                     }
                     labelPlacement="start"
                 />
-                {/* Continue com os outros segmentos... */}
+                 <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Supermercado')} onChange={() => toggleSegment('Supermercado')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Supermercado
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de diversos produtos
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                /> 
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Bufê')} onChange={() => toggleSegment('Bufê')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Bufê
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Produtos relacionados a grãos e sementes
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Doceria')} onChange={() => toggleSegment('Doceria')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Doceria
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de doces e sobremesas
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Padaria')} onChange={() => toggleSegment('Padaria')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Padaria
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de pães
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Confeitaria')} onChange={() => toggleSegment('Confeitaria')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Confeitaria
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de tortas e bolos
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Cafeteria')} onChange={() => toggleSegment('Cafeteria')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Cafeteria
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de cafés e chás
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Peixaria')} onChange={() => toggleSegment('Peixaria')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Peixaria
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de peixes e frutos do mar
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Adega')} onChange={() => toggleSegment('Adega')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Adega
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de bebidas alcoólicas
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Fast Food')} onChange={() => toggleSegment('Fast Food')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Fast Food
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de sanduíches
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Bar')} onChange={() => toggleSegment('Bar')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Bar
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de bebidas e drinks
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<CustomCheckbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={selectedSegments.includes('Sorveteria')} onChange={() => toggleSegment('Sorveteria')} />}
+                    label={
+                        <LabelContent>
+                            <Typography variant="body1" color="text-foreground">
+                                Sorveteria
+                            </Typography>
+                            <Typography variant="caption" color="text-foreground">
+                                Focado na venda de sorvetes e picolés
+                            </Typography>
+                        </LabelContent>
+                    }
+                    labelPlacement="start"
+                />
+
+        {/* Continue com os outros segmentos... */}
             </FormGroup>
         </FormControl>
     );
